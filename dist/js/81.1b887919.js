bd1bda5a00bdd10b310519c2afb6b722ac4731cb
Александр Невский <sashnevski@MacBook-Air-Aleksandr.local>
2022-04-29 02:00:32 +0300

    Initial dist subtree commit

---
+++ dist/js/81.1b887919.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvuejs_test"]=self["webpackChunkvuejs_test"]||[]).push([[81],{5081:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var s=n(3396);const a=(0,s._)("div",{class:"container"},[(0,s._)("h1",{class:"section-title"},"Системы хранения")],-1),r=[a];function u(e,t,n,a,u,c){return(0,s.wg)(),(0,s.iD)("main",null,r)}var c={name:"StorageSystemsPage"},i=n(89);const l=(0,i.Z)(c,[["render",u]]);var o=l}}]);
+//# sourceMappingURL=81.1b887919.js.map
